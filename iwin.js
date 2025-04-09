50163b0b290531cb4973052b98930c974be7f14f
ming yang shih <39481555+mingyangshih@users.noreply.github.com>
2025-04-09 16:36:06 +0800

    iwin session done

--- iwin.js
+++ iwin.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 var _ = require("lodash");
 const csv = require("csv-parser");
-let file = "./iwin/iwin_0301_one_user.csv";
+let file = "./iwin/iwin_03-31_Yolla.csv";
 let reportResult = [];
 
 fs.createReadStream(file)
@@ -14,11 +14,7 @@ fs.createReadStream(file)
     const { game, nonGame } = _.groupBy(reportResult, (item) =>
       item.pagepath.includes("/online-games/game/play/") ? "game" : "nonGame"
     );
-    console.log(
-      _.groupBy(reportResult, (item) =>
-        item.pagepath.includes("/online-games/game/play/") ? "game" : "nonGame"
-      )["nonGame"]
-    );
+
     // First get non-game revenue by user (simplified)
     let nonGameUserRevenue = _.chain(nonGame)
       .groupBy("uuid")
@@ -29,6 +25,20 @@ fs.createReadStream(file)
         };
       })
       .keyBy("uuid")
+      .value();
+
+    // Calculate session numbers for each pagepath
+    const pageSessionCounts = _.chain(game)
+      .groupBy("pagepath")
+      .mapValues((pageEvents) => {
+        return _.uniqBy(
+          pageEvents.map((event) => ({
+            uuid: event.uuid,
+            sessionid: event.sessionid,
+          })),
+          (item) => `${item.uuid}_${item.sessionid}`
+        ).length;
+      })
       .value();
 
     // Process game pages and add non-game revenue
@@ -96,6 +106,7 @@ fs.createReadStream(file)
         return {
           uuid,
           pagepath,
+          sessionCount: pageSessionCounts[pagepath] || 0,
           timeSpentMinutes: Math.round(totalTimeSpentMinutes * 100) / 100,
           eventCount: events.length,
           ...revenue,
@@ -108,6 +119,7 @@ fs.createReadStream(file)
         totalEvents: _.sumBy(users, "eventCount"),
         avgTimeSpent:
           Math.round(_.meanBy(users, "timeSpentMinutes") * 100) / 100,
+        sessionCount: pageSessionCounts[pagepath] || 0,
         revenue_1min: Math.round(_.sumBy(users, "1min") * 100) / 100,
         revenue_5min: Math.round(_.sumBy(users, "5min") * 100) / 100,
         revenue_10min: Math.round(_.sumBy(users, "10min") * 100) / 100,
@@ -122,17 +134,17 @@ fs.createReadStream(file)
     // Save game pages report
     const gameCsvOutput = gameRevenueReport.map(
       (stat) =>
-        `${stat.pagepath},${stat.uniqueUsers},${stat.totalEvents},${stat.avgTimeSpent},` +
+        `${stat.pagepath},${stat.uniqueUsers},${stat.totalEvents},${stat.avgTimeSpent},${stat.sessionCount},` +
         `${stat.revenue_1min},${stat.revenue_5min},${stat.revenue_10min},${stat.revenue_20min},` +
         `${stat.revenue_30min},${stat.revenue_40min},${stat.revenue_50min},${stat.revenue_60min}`
     );
     gameCsvOutput.unshift(
-      "pagepath,uniqueUsers,totalEvents,avgTimeSpent," +
+      "pagepath,uniqueUsers,totalEvents,avgTimeSpent,sessionCount," +
         "revenue_1min,revenue_5min,revenue_10min,revenue_20min," +
         "revenue_30min,revenue_40min,revenue_50min,revenue_60min"
     );
     fs.writeFileSync(
-      "./iwin/iwin_03-31_Yolla_revenue_report.csv",
+      "./iwin/iwin_03-31_Yolla_revenue_report_session.csv",
       gameCsvOutput.join("\n")
     );
   });
