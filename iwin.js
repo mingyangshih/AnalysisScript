34e1343142c7d409e39547045acf24b9969a97ee
ming yang shih <39481555+mingyangshih@users.noreply.github.com>
2025-07-16 10:15:34 +0800

    update script

--- iwin.js
+++ iwin.js
@@ -22,32 +22,6 @@ function processData(reportResult, fileName = "test") {
     .keyBy("uuid")
     .value();
 
-<<<<<<< HEAD
-  // Calculate session numbers for each pagepath
-  const pageSessionCounts = _.chain(game)
-    .groupBy("pagepath")
-    .mapValues((pageEvents) => {
-      return _.uniqBy(
-        pageEvents.map((event) => ({
-          uuid: event.uuid,
-          sessionid: event.sessionid,
-        })),
-        (item) => `${item.uuid}_${item.sessionid}`
-      ).length;
-    })
-    .value();
-
-  const totalEventValue = 23291796;
-  const dfpRevenue = 329.9636;
-
-  // Process game pages and add non-game revenue
-  let gameRevenueReport = _.chain(game)
-    .groupBy((item) => `${item.uuid}_${item.pagepath}_${item.sessionid}`)
-    .map((events) => {
-      const pagepath = events[0].pagepath;
-      const uuid = events[0].uuid;
-
-=======
   // Calculate unique users for each time bucket
   const pageUniqueUsers = _.chain(game)
     .groupBy(
@@ -59,7 +33,6 @@ function processData(reportResult, fileName = "test") {
       const uuid = events[0].uuid;
       const country = events[0].country;
       const devicecategory = events[0].devicecategory;
->>>>>>> only_pagepath
       let revenue = {
         "1min": 0,
         "5min": 0,
@@ -81,10 +54,6 @@ function processData(reportResult, fileName = "test") {
           const cm12Minutes = (Number(event.cm12) || 0) / 60;
           const eventValue = Number(event.value) || 0;
 
-<<<<<<< HEAD
-          // Add revenue to all applicable buckets based on cm12Minutes
-=======
->>>>>>> only_pagepath
           if (totalTimeSpentMinutes > 1 && cm12Minutes <= 1)
             revenue["1min"] += eventValue;
           if (totalTimeSpentMinutes > 5 && cm12Minutes <= 5)
@@ -103,15 +72,8 @@ function processData(reportResult, fileName = "test") {
             revenue["60min"] += eventValue;
         });
 
-<<<<<<< HEAD
-        // Add non-game revenue for this user to qualified buckets only
-        const userNonGameRevenue = nonGameUserRevenue[uuid]?.totalRevenue || 0;
-
-        // Only add non-game revenue to buckets where game revenue is not zero
-=======
         // Add non-game revenue
         const userNonGameRevenue = nonGameUserRevenue[uuid]?.totalRevenue || 0;
->>>>>>> only_pagepath
         if (revenue["1min"] > 0) revenue["1min"] += userNonGameRevenue;
         if (revenue["5min"] > 0) revenue["5min"] += userNonGameRevenue;
         if (revenue["10min"] > 0) revenue["10min"] += userNonGameRevenue;
@@ -125,72 +87,6 @@ function processData(reportResult, fileName = "test") {
       return {
         uuid,
         pagepath,
-<<<<<<< HEAD
-        sessionNumber: pageSessionCounts[pagepath],
-        timeSpentMinutes: Math.round(totalTimeSpentMinutes * 100) / 100,
-        eventCount: events.length,
-        ...revenue,
-      };
-    })
-    .groupBy("pagepath")
-    .map((sessions, pagepath) => ({
-      pagepath,
-      totalUniqueUsers: _.uniqBy(sessions, "uuid").length,
-      totalEvents: _.sumBy(sessions, "eventCount"),
-      avgTimeSpent:
-        Math.round(_.meanBy(sessions, "timeSpentMinutes") * 100) / 100,
-      sessionCount: sessions.length,
-      // Revenue calculation
-      revenue_1min:
-        Math.round(
-          (_.sumBy(sessions, "1min") / totalEventValue) * dfpRevenue * 100
-        ) / 100,
-      revenue_5min:
-        Math.round(
-          (_.sumBy(sessions, "5min") / totalEventValue) * dfpRevenue * 100
-        ) / 100,
-      revenue_10min:
-        Math.round(
-          (_.sumBy(sessions, "10min") / totalEventValue) * dfpRevenue * 100
-        ) / 100,
-      revenue_20min:
-        Math.round(
-          (_.sumBy(sessions, "20min") / totalEventValue) * dfpRevenue * 100
-        ) / 100,
-      revenue_30min:
-        Math.round(
-          (_.sumBy(sessions, "30min") / totalEventValue) * dfpRevenue * 100
-        ) / 100,
-      revenue_40min:
-        Math.round(
-          (_.sumBy(sessions, "40min") / totalEventValue) * dfpRevenue * 100
-        ) / 100,
-      revenue_50min:
-        Math.round(
-          (_.sumBy(sessions, "50min") / totalEventValue) * dfpRevenue * 100
-        ) / 100,
-      revenue_60min:
-        Math.round(
-          (_.sumBy(sessions, "60min") / totalEventValue) * dfpRevenue * 100
-        ) / 100,
-    }))
-    .value();
-
-  // Save game pages report
-  const gameCsvOutput = gameRevenueReport.map(
-    (stat) =>
-      `${stat.pagepath},${stat.totalUniqueUsers},${stat.totalEvents},${stat.avgTimeSpent},` +
-      `${stat.revenue_1min},${stat.revenue_5min},${stat.revenue_10min},${stat.revenue_20min},` +
-      `${stat.revenue_30min},${stat.revenue_40min},${stat.revenue_50min},${stat.revenue_60min}`
-  );
-  gameCsvOutput.unshift(
-    "pagepath,totalUniqueUsers,totalEvents,avgTimeSpent," +
-      "revenue_1min,revenue_5min,revenue_10min,revenue_20min," +
-      "revenue_30min,revenue_40min,revenue_50min,revenue_60min"
-  );
-
-  fs.writeFileSync(`./iwin/iwin_03-31_report.csv`, gameCsvOutput.join("\n"));
-=======
         country,
         devicecategory,
         revenue,
@@ -467,7 +363,6 @@ function processData(reportResult, fileName = "test") {
     `./iwin/iwin_03-01_report_country_device_revenue_session.csv`,
     gameCsvOutput.join("\n")
   );
->>>>>>> only_pagepath
 }
 
 fs.createReadStream(file)
